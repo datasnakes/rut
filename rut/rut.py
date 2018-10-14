28bb15406b4d7b43a4f8eea26e4a3f8f528faa24
Bruno Grande <bgrande@sfu.ca>
2018-10-14 15:06:42 -0700

    Add support for versions and modularize code

--- rut/rut.py
+++ rut/rut.py
@@ -1,4 +1,4 @@
-from .utils import run_r_command, to_vector
+from .utils import run_r_command, to_logical, to_character, to_vector, install_r_package
 import click
 
 
@@ -16,10 +16,45 @@ def install():
 @click.argument("packages", type=str, nargs=-1)
 @click.option("--repos", "-r", default="http://cloud.r-project.org/",
               help="CRAN repository")
-def cran(packages, repos):
-    """Install packages from CRAN."""
-    vector = to_vector(packages)
-    cmd = ["remotes::install_cran(", vector, ", repos='", repos, "')"]
-    cmd = "".join(cmd)
-    click.echo(f"Running in R: {cmd}")
-    run_r_command(cmd)
+@click.option("--force", "-f", default=False,
+              help="Force install package")
+@click.option("--upgrade", "-u", default=False,
+              help="Upgrade out-of-date packages")
+def cran(packages, repos, force, upgrade):
+    """Install packages from CRAN.
+
+    Specify versions using the following format: pkgA=1.4 pkgB=2.1
+    """
+
+    # Format values for R
+    repos_fmt = to_character(repos)
+    force_fmt = to_logical(force)
+    upgrade_fmt = to_logical(upgrade)
+
+    # Make sure the remotes R package is installed
+    install_r_package("remotes", repos)
+
+    # Separate packages with specified versions
+    v_pkgs = []  # With versions
+    unv_pkgs = []  # Without versions
+    for pkg in packages:
+        if "=" in pkg:
+            v_pkgs.append(pkg)
+        else:
+            unv_pkgs.append(pkg)
+
+    # Install unversioned packages first
+    if unv_pkgs:
+        unv_pkgs_fmt = to_vector(unv_pkgs)
+        cmd = (f"remotes::install_cran({unv_pkgs_fmt}, repos = {repos_fmt}, "
+               f"upgrade = {upgrade_fmt}, force = {force_fmt})")
+        run_r_command(cmd)
+
+    # Install/update versioned packages next
+    for pkg in v_pkgs:
+        package, version = pkg.split("=", 1)
+        package_fmt = to_character(package)
+        version_fmt = to_character(version)
+        cmd = (f"remotes::install_version({package_fmt}, {version_fmt}, repos = {repos_fmt}, "
+               f"upgrade = {upgrade_fmt}, force = {force_fmt})")
+        run_r_command(cmd)
--- rut/utils.py
+++ rut/utils.py
@@ -1,24 +1,57 @@
 from shutil import which
 from os.path import exists
 from subprocess import run
+import click
 
 
 def in_path(program):
-	"""Check if R is available in PATH."""
-	return which(program) is not None
+    """Check if R is available in PATH."""
+    return which(program) is not None
 
 
 def run_r_command(cmd, program="R"):
-	"""Run R command"""
-	if not exists(program) and not in_path(program):
-		raise Exception(f"{program} does not exists, nor is it available "
-						"in the PATH environment variable.")
-	process = run([program, "-e", cmd])
-	return process
+    """Run R command"""
+    if not exists(program) and not in_path(program):
+        raise Exception(f"{program} does not exists, nor is it available "
+                        "in the PATH environment variable.")
+    args = [program, "--quiet", "-e", cmd]
+    click.echo(f"$ {' '.join(args)}")
+    process = run(args)
+    return process
 
 
-def to_vector(iterable):
-	"""Convert iterable to a R format vector."""
-	vector = "', '".join(str(e) for e in iterable)
-	vector = "c('" + vector + "')"
-	return vector
+def install_r_package(package, repos):
+    """Check if R package is installed and if not, install it.
+
+    Returns whether the package was initially installed
+    """
+    package_fmt = to_character(package)
+    repos_fmt = to_character(repos)
+    check_proc = run_r_command(f"library({package_fmt})")
+    is_installed = check_proc.returncode == 0
+    if not is_installed:
+        install_proc = run_r_command(f"install.packages({package_fmt}, repos = {repos_fmt})")
+        if install_proc.returncode != 0:
+            raise Exception("{package} didn't install correctly.")
+    return is_installed
+
+
+def to_logical(value):
+    """Format boolean value to logical for R."""
+    return "TRUE" if value else "FALSE"
+
+
+def to_character(value):
+    """Format string value to character for R."""
+    return "'" + value + "'"
+
+
+def to_vector(iterable, type="character"):
+    """Convert iterable to vector for R."""
+    formatters = {
+        "character": to_character,
+        "logical": to_logical}
+    formatter = formatters[type]
+    vector = ", ".join(formatter(v) for v in iterable)
+    vector = "c(" + vector + ")"
+    return vector
