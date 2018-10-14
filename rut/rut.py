9f4111644f1d0564de5043cda13356f1c37de8f2
Bruno Grande <bgrande@sfu.ca>
2018-10-13 17:16:20 -0700

    Implement Python CLI for remotes::install_cran()

--- rut/rut.py
+++ rut/rut.py
@@ -1,6 +1,25 @@
+from .utils import run_r_command, to_vector
 import click
 
-@click.command()
-@click.option('--test', '-t', default='', help='What is this?')
-def cli():
-    click.echo("This is a test of the rut cli!")
+
+@click.group()
+def rut():
+    pass
+
+
+@rut.group()
+def install():
+    pass
+
+
+@install.command()
+@click.argument("packages", type=str, nargs=-1)
+@click.option("--repos", "-r", default="http://cloud.r-project.org/",
+              help="CRAN repository")
+def cran(packages, repos):
+    """Install packages from CRAN."""
+    vector = to_vector(packages)
+    cmd = ["remotes::install_cran(", vector, ", repos='", repos, "')"]
+    cmd = "".join(cmd)
+    click.echo(f"Running in R: {cmd}")
+    run_r_command(cmd)
