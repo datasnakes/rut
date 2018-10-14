991b2bbda3623a6d957618c8f3969d18b16b35c5
sdhutchins <sdhutchins@outlook.com>
2018-10-14 14:52:08 -0500

    Updated cli for remotes

--- rut/rut.py
+++ rut/rut.py
@@ -1,4 +1,5 @@
-from .utils import run_r_command, to_vector
+from rut.utils import run_r_command, to_vector
+from rut.core import install_remotes
 import click
 
 
@@ -17,9 +18,10 @@ def install():
 @click.option("--repos", "-r", default="http://cloud.r-project.org/",
               help="CRAN repository")
 def cran(packages, repos):
-    """Install packages from CRAN."""
+    """Install packages from CRAN with remotes"""
     vector = to_vector(packages)
     cmd = ["remotes::install_cran(", vector, ", repos='", repos, "')"]
     cmd = "".join(cmd)
     click.echo(f"Running in R: {cmd}")
+    install.remotes()
     run_r_command(cmd)
